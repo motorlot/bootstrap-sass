7ee2b55c967e023a066482f1caf9177838c24b72
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2019-08-22 18:42:00 +0300

    Use SassC instead of Sass.

--- lib/bootstrap-sass/compass_functions.rb
+++ lib/bootstrap-sass/compass_functions.rb
@@ -1,14 +1,14 @@
 # This contains functions for use with a project *only* using Compass.
-module Sass::Script::Functions
+module SassC::Script::Functions
   def image_path(source, options = {})
     if defined?(::Sprockets)
-      ::Sass::Script::String.new sprockets_context.image_path(source.value).to_s, :string
+      ::SassC::Script::String.new sprockets_context.image_path(source.value).to_s, :string
     elsif defined?(::Compass)
-      image_url(source, Sass::Script::Bool.new(true))
+      image_url(source, SassC::Script::Bool.new(true))
     else
       # Revert to the old compass-agnostic path determination
       asset_sans_quotes = source.value.gsub('"', '')
-      Sass::Script::String.new("/images/#{asset_sans_quotes}", :string)
+      SassC::Script::String.new("/images/#{asset_sans_quotes}", :string)
     end
   end
 
--- lib/bootstrap-sass/sass_functions.rb
+++ lib/bootstrap-sass/sass_functions.rb
@@ -1,6 +1,6 @@
-require 'sass'
+require 'sassc'
 
-module Sass::Script::Functions
+module SassC::Script::Functions
   # LARS: Snatched from compass - 2011-11-29 - used for gradients in IE6-9
   # returns an IE hex string for a color with an alpha channel
   # suitable for passing to IE filters.
@@ -8,7 +8,7 @@ module Sass::Script::Functions
     assert_type color, :Color
     alpha = (color.alpha * 255).round
     alphastr = alpha.to_s(16).rjust(2, '0')
-    Sass::Script::String.new("##{alphastr}#{color.send(:hex_str)[1..-1]}".upcase)
+    SassC::Script::String.new("##{alphastr}#{color.send(:hex_str)[1..-1]}".upcase)
   end
   declare :ie_hex_str, [:color]
 end
