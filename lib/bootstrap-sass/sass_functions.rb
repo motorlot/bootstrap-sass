d24aad92bf682b40fcb424628b725cb4d6c8211e
Vladimir Krivchenko <vladimir.krivchenko@gmail.com>
2019-08-22 18:47:19 +0300

    Revert "Use SassC instead of Sass."
    This reverts commit 7ee2b55c967e023a066482f1caf9177838c24b72.

--- lib/bootstrap-sass/sass_functions.rb
+++ lib/bootstrap-sass/sass_functions.rb
@@ -1,6 +1,6 @@
-require 'sassc'
+require 'sass'
 
-module SassC::Script::Functions
+module Sass::Script::Functions
   # LARS: Snatched from compass - 2011-11-29 - used for gradients in IE6-9
   # returns an IE hex string for a color with an alpha channel
   # suitable for passing to IE filters.
@@ -8,7 +8,7 @@ module SassC::Script::Functions
     assert_type color, :Color
     alpha = (color.alpha * 255).round
     alphastr = alpha.to_s(16).rjust(2, '0')
-    SassC::Script::String.new("##{alphastr}#{color.send(:hex_str)[1..-1]}".upcase)
+    Sass::Script::String.new("##{alphastr}#{color.send(:hex_str)[1..-1]}".upcase)
   end
   declare :ie_hex_str, [:color]
 end
